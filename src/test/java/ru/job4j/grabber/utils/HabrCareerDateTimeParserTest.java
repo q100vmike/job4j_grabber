171fab3b9890c9b5e679559cec458e1c8e7b19f6
q100vmike <melkin@mail.ru>
2024-10-08 12:42:52 +0300

    2.1. Преобразование даты [#289476]

--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -9,7 +9,7 @@ class HabrCareerDateTimeParserTest {
 
     @Test
     public void formatDateTime1isTrue() {
-        HabrCareerDateTimeParser parser= new HabrCareerDateTimeParser ();
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         String input = "2024-09-18T11:03:12+03:00";
         String expected = "2024-09-18T11:03:12";
         LocalDateTime result = parser.parse(input);
@@ -17,7 +17,7 @@ class HabrCareerDateTimeParserTest {
     }
     @Test
     public void formatDateTime2isTrue() {
-        HabrCareerDateTimeParser parser= new HabrCareerDateTimeParser ();
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         String input = "2024-10-06T10:07:35+03:00";
         String expected = "2024-10-06T10:07:35";
         LocalDateTime result = parser.parse(input);
@@ -25,7 +25,7 @@ class HabrCareerDateTimeParserTest {
     }
     @Test
     public void formatDateTime3isTrue() {
-        HabrCareerDateTimeParser parser= new HabrCareerDateTimeParser ();
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
         String input = "2024-10-05T11:56:42+03:00";
         String expected = "2024-10-05T11:56:42";
         LocalDateTime result = parser.parse(input);
